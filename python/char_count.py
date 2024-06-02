f717a5d855dfa2e062e921b7663a0a0b106c0ba6
Crusader-RG <justin.leeper92@gmail.com>
2024-06-01 20:24:29 -0600

    complete

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,15 @@
 def char_count(str):
-  # Your code here
+  ans_dic = {}
+  #create dictionary from string
+  for let in str:
+    if let == " ":
+      continue
+    elif let in ans_dic.keys():
+      ans_dic[let] += 1
+    else:
+      ans_dic[let] = 1
+  #sort dictionary by values in descending order
+  sorted_dict = dict(sorted(ans_dic.items(), key=lambda item: item[1], reverse=True))
+  
+  return sorted_dict
+
